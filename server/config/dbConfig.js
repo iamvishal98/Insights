e5fed317ba618e3b221409988f66d4f384855851
Vishal Chauhan <vishalchauhan@Vishals-MacBook-Air.local>
2023-06-21 08:30:31 +0530

    initial setup

---
+++ server/config/dbConfig.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+const connectDb = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`DB CONNECTED : ${conn.connection.host}`);
+  } catch (error) {
+    console.log(`DB CONNECTION ERROR: ${error}`);
+    process.exit(1);
+  }
+};
+
+export default connectDb;
---
+++ server/server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import clientRoutes from "./routes/client.js";
+import generalRoutes from "./routes/general.js";
+import managementRoutes from "./routes/management.js";
+import salesRoutes from "./routes/sales.js";
+import connectDb from "./config/dbConfig.js";
+
+//CONFIGURATIONS
+dotenv.config();
+connectDb();
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(cors());
+
+//ROUTES
+app.use("/client", clientRoutes);
+app.use("/general", generalRoutes);
+app.use("/management", managementRoutes);
+app.use("/sales", salesRoutes);
+
+const PORT = process.env.PORT || 9090;
+app.listen(PORT, () => {
+  console.log(`server listening on ${PORT}`);
+});
